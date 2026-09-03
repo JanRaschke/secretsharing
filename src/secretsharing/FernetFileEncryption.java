58d834d5fdac19ca99cc941ed122c0e3b7079234
Nils <nils.gellert@outlook.de>
2026-09-03 09:25:21 +0200

    NewFile

---
+++ src/secretsharing/FernetFileEncryption.java
@@ -0,0 +1,54 @@
+package secretsharing;
+
+import com.macasaet.fernet.Key;
+import com.macasaet.fernet.Token;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+public class FernetFileEncryption {
+
+    private static final SecureRandom rng = new SecureRandom();
+
+    /**
+     * Erzeugt einen zufälligen 256-Bit (32-Byte) Fernet-Schlüssel
+     * 
+     * @return Ein neues Fernet Key-Objekt
+     */
+    public static Key generateKey() {
+        // TODO
+        return null;
+    }
+
+    /**
+     * 
+     * @param inputFile Die zu verschlüsselnde Datei F
+     * @param key       Der Fernet-Schlüssel
+     * @return Die erzeugte Datei F.enc
+     * @throws IOException Wenn beim Lesen oder Schreiben ein Fehler auftritt
+     */
+    public static File encrypt(File inputFile, Key key) throws IOException {
+        // TODO
+        return null;
+    }
+
+    /**
+     * 
+     * @param encFile Die verschlüsselte Datei F.enc
+     * @param key     Der Fernet-Schlüssel
+     * @return Die erzeugte Datei F.dec
+     * @throws IOException Wenn beim Lesen oder Schreiben ein Fehler auftritt
+     */
+    public static File decrypt(File encFile, Key key) throws IOException {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // TODO
+    }
+
+}
